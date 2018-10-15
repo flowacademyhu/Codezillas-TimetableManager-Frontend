2c28ca91348e743c9b169ec4dfc7d223f7e8c046
g4bor <gabor.tanacs@yahoo.com>
2018-10-15 21:07:04 +0200

    add interceptor & sessionStorage(with token, roles)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
@@ -9,7 +9,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
- import { TimeTableComponent } from './components/time-table/time-table.component';
+import { TimeTableComponent } from './components/time-table/time-table.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AuthService } from './services/auth.service';
@@ -21,6 +21,7 @@ import { GroupComponent } from './components/group/group.component';
 import { ButtonColorDirective } from './button-color.directive';
 import { UsersComponent } from './components/users/users.component';
 import { SubjectService } from './services/subject.service';
+import { SpringbootInterceptor } from './services/interceptor.service';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
@@ -35,7 +36,7 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-     TimeTableComponent,
+    TimeTableComponent,
     RegistrationComponent,
     SidebarComponent,
     SubjectComponent,
@@ -54,7 +55,11 @@ const appRoutes: Routes = [
     DxButtonModule,
     NgbModule
   ],
-  providers: [AuthService, ClassService, GroupService, SubjectService, UserService],
+  providers: [AuthService, ClassService, GroupService, SubjectService, UserService,{
+    provide: HTTP_INTERCEPTORS,
+    useClass: SpringbootInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
